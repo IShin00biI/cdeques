3504eca342c4c53574b4d999827347f1d0c8c3ae
Michał Naruniec <mn360386@students.mimuw.edu.pl>
2016-10-24 17:24:43 +0200

    Inicjalizacja stalej kolejki

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# cdeques
-JNP1 Zadanie 2
+*trzeba gdzies zainicjowac strumienie(czytanka)
+
+*initialization fiasco chyba zrobione
+
+*chyba dobrze by bylo opakowac strumien diagnostyczny, ale trzeba oddzielnie w obu implementacjach (?)
+
+*linie wyjscia diagnostycznego sa takie jak w przykladzie, ale ich kombinacje/czestotliwosc moga sie roznic
---
+++ src/strdequeconst.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+#include "strdeque.h"
+
+#ifndef NDEBUG
+const bool DEBUG = true;
+#else
+const bool DEBUG = false;
+#endif
+
+static unsigned long init_emptystrdeque() {
+	if(DEBUG) std::cerr << "strdequeconst init invoked" << std::endl;
+	unsigned long id = strdeque_new();
+	std::cerr << "strdeque_const init finished" << std::endl;
+	return id;
+}
+
+extern unsigned long emptystrdeque() {
+	if(DEBUG) std::cerr << "emptystrdeque()" << std::endl;
+	static const unsigned long id = init_emptystrdeque();
+	return id;
+}
